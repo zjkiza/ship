b120f884d6f56cccbb91e1a57d19c9c82cf31fac
Zoran Jankovic <zjkiza@gmail.com>
2019-11-23 12:23:08 +0100

    Add auth.admin middleware

---
+++ app/Http/Middleware/AccessAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\User;
+use Closure;
+
+class AccessAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (auth()->check() && auth()->user()->role === User::ADMIN) {
+            return $next($request);
+        }
+
+        return redirect()->route('index-request');
+    }
+}
