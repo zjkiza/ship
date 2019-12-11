1bd63deb175ccbafcaf507c5fb81a386beaec332
Zoran Jankovic <zoran.jankovic@quantox.com>
2019-12-11 09:09:58 +0100

    Add admin user to seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -10,6 +10,23 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin'),
+            'role' => User::ADMIN,
+        ]);
+
+        $rank = \App\Models\Rank::where('name', 'captain')->first();
+        $ship = \App\Models\Ship::first();
+        \App\Models\Craw::create([
+            'name' => 'Jon',
+            'sur_name' => 'Do',
+            'user_id' => $user->id,
+            'rank_id' => $rank->id,
+            'ship_id' => $ship->serial_number,
+        ]);
+
         factory(User::class, 10)->create();
     }
 }
