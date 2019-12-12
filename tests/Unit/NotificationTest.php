b4e8822e462a1c6efdfab70451ee193c8d17bf8b
Zoran Jankovic <zoran.jankovic@quantox.com>
2019-12-12 09:30:03 +0100

    Add test for test values in notification relations

--- tests/Unit/NotificationTest.php
+++ tests/Unit/NotificationTest.php
@@ -13,23 +13,49 @@ class NotificationTest extends TestCase
     use DatabaseTransactions;
 
     private $notification;
+    private $ship;
+    /** @var Rank */
+    private $rank;
 
     public function setUp(): void
     {
         parent::setUp();
 
-        $this->notification = factory(Notification::class)->create();
+        $this->rank = factory(Rank::class)->create();
+        $this->ship = factory(Ship::class)->create();
+        $this->notification = factory(Notification::class)->create([
+                'rank_id' => $this->rank->id,
+                'ship_id' => $this->ship->serial_number,
+        ]);
     }
 
     /** @test */
-    public function a_notification_has_a_user(): void
+    public function a_notification_has_a_rank(): void
     {
         $this->assertInstanceOf(Rank::class, $this->notification->rank);
+    }
+
+    /** @test */
+    public function a_notification_has_a_rank_values(): void
+    {
+        $this->assertEquals(
+            $this->rank->toArray(),
+            $this->notification->rank->toArray()
+        );
     }
 
     /** @test */
     public function a_notification_has_a_ship(): void
     {
         $this->assertInstanceOf(Ship::class, $this->notification->ship);
+    }
+
+    /** @test */
+    public function a_notification_has_a_ship_values(): void
+    {
+        $this->assertEquals(
+            $this->ship->toArray(),
+            $this->notification->ship->toArray()
+        );
     }
 }
